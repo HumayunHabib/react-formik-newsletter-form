80a8b5042e76f14c4138ba58cd585429264f0e49
HumayunHabib <humayun.habib@magnusmage.com>
2022-08-25 18:10:20 +0500

    use of getFieldProps() instead explicit props on input

--- src/NewsLetterForms/SignupForm.js
+++ src/NewsLetterForms/SignupForm.js
@@ -29,9 +29,7 @@ const SignupForm = () => {
         id="firstName"
         name="firstName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
+        {...formik.getFieldProps("firstName")}
       />
       {formik.touched.firstName && formik.errors.firstName ? (
         <div>{formik.errors.firstName}</div>
@@ -42,9 +40,7 @@ const SignupForm = () => {
         id="lastName"
         name="lastName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
+        {...formik.getFieldProps("lastName")}
       />
       {formik.touched.lastName && formik.errors.lastName ? (
         <div>{formik.errors.lastName}</div>
@@ -55,9 +51,7 @@ const SignupForm = () => {
         id="email"
         name="email"
         type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
+        {...formik.getFieldProps("email")}
       />
       {formik.touched.email && formik.errors.email ? (
         <div>{formik.errors.email}</div>
