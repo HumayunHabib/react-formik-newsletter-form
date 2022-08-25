d241e308af2ce2bde89a6dc6241875842e0e927c
HumayunHabib <humayun.habib@magnusmage.com>
2022-08-25 17:48:19 +0500

    tracking touched

--- src/NewsLetterForms/SignupForm.js
+++ src/NewsLetterForms/SignupForm.js
@@ -48,7 +48,9 @@ const SignupForm = () => {
         onBlur={formik.handleBlur}
         value={formik.values.firstName}
       />
-      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -59,7 +61,9 @@ const SignupForm = () => {
         onBlur={formik.handleBlur}
         value={formik.values.lastName}
       />
-      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
 
       <label htmlFor="email">Email Address</label>
       <input
@@ -70,7 +74,9 @@ const SignupForm = () => {
         onBlur={formik.handleBlur}
         value={formik.values.email}
       />
-      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <button type="submit">Submit</button>
     </form>
