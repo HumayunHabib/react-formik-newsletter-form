df98e15753fbfd5f6141a751cc653227baf48432
HumayunHabib <humayun.habib@magnusmage.com>
2022-08-25 18:42:17 +0500

    using field examples ----commented

--- src/NewsLetterForms/SignupForm.js
+++ src/NewsLetterForms/SignupForm.js
@@ -32,6 +32,15 @@ const SignupForm = () => {
         <label htmlFor="email">Email Address</label>
         <Field name="email" type="email" />
         <ErrorMessage name="email" />
+        {/* <Field name="firstName" className="form-input" placeholder="Jane" /> */}
+        {/* <textarea className="form-textarea"/></textarea> */}
+        {/* <Field name="message" as="textarea" className="form-textarea" /> */}
+        {/* // <select className="my-select"/> */}
+        {/* <Field name="colors" as="select" className="my-select">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </Field> */}
         <button type="submit">Submit</button>
       </Form>
     </Formik>
