1f2c37a002f2be055754539e3a13f3ac80781773
HumayunHabib <humayun.habib@magnusmage.com>
2022-08-25 17:20:20 +0500

    validate function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import SignupForm from "./NewsLetterForms/SignupForm";
-import SimpleForm from "./SimpleForm";
+// import SimpleForm from "./SimpleForm";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      {/* <SimpleForm/> */}
       <SignupForm />
-      {/* </header> */}
     </div>
   );
 }
